20e6cae7ad994080df9241273573604d4f564348
KennOtieno <otiken001@gmail.com>
2025-04-16 05:38:50 -0700

    layout/default

--- layout/default.vue
+++ layout/default.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
+        <ul>
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="articles">Articles</NuxtLink></li>
-        <li><NuxtLink to="Register">Register</NuxtLink></li>
+        <li><NuxtLink to="register">Register</NuxtLink></li>
+        </ul>
       <slot />
     </div>
   </template>
