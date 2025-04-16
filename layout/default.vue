bb7330e934d995c4f6a4a9e6684163ac79b539ef
KennOtieno <otiken001@gmail.com>
2025-04-16 05:10:00 -0700

    Still in Progress

---
+++ layout/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="articles">Articles</NuxtLink></li>
+        <li><NuxtLink to="Register">Register</NuxtLink></li>
+      <slot />
+    </div>
+  </template>
+  
